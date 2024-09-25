2ea216449dc15672c13b5740c496b423b7af4b37
pbson <phambason639@gmail.com>
2024-09-25 11:06:48 +0100

    fix: update base api env

--- src/services/api/baseApi.ts
+++ src/services/api/baseApi.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const baseApi = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:1337',
   timeout: 10000,
   headers: {
     Accept: 'applications/json',
